dcf9549eee31c9fe9d8609eb6420ffc0e69f1fad
Waled Karim <waledkarim445@gmail.com>
2023-10-04 02:30:00 +0600

    Created a folder and added clients controller and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientModule } from './clients/client/client.module';
+import { ClientsModule } from './clients/clients.module';
 
 @Module({
-  imports: [ClientModule],
+  imports: [ClientModule, ClientsModule],
   controllers: [AppController],
   providers: [AppService],
 })
