0790ee6f4b7d871cf7846e33e57af47acd8b6900
hatem3696 <abdulhayee5767@gmail.com>
2023-10-26 20:16:09 +0600

    Structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { AdminModule } from './admin/admin.module';
 import { FreelancerModule } from './freelancer/freelancer.module';
 import { ModeratorModule } from './moderator/moderator.module';
+import { AuthController } from './shared/auth/auth.controller';
+import { AuthModule } from './shared/auth/auth.module';
 
 @Module({
-  imports: [AdminModule, FreelancerModule, ModeratorModule],
-  controllers: [AppController],
+  imports: [AdminModule, FreelancerModule, ModeratorModule, AuthModule],
+  controllers: [AppController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}
