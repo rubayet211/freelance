ab49cb39937bf42117530f58f47ca7b68887a46e
hatem3696 <abdulhayee5767@gmail.com>
2023-10-26 20:19:56 +0600

    Structure #1

--- src/shared/auth/auth.controller.ts
+++ src/shared/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor() {}
+  @Post('login')
+  async login() {
+    return { message: 'Login' };
+  }
+  @Post('register')
+  async register() {
+    return { message: 'Register' };
+  }
+}
--- src/shared/auth/auth.service.ts
+++ src/shared/auth/auth.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async login() {
+    return { message: 'Login' };
+  }
+  async register() {
+    return { message: 'Register' };
+  }
+  async validateUser() {
+    return { message: 'Validate User' };
+  }
+}
